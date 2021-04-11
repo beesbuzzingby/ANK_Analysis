f8526bd1a3e330ed9aff4162ccf700db1e59c811
Carissa Garde <47163038+beesbuzzingby@users.noreply.github.com>
2021-04-11 07:37:46 -0400

    Add files via upload

--- linear_reg_analysis_2_6.py
+++ linear_reg_analysis_2_6.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 from csv import reader
 from csv import writer
-import numpy
+import numpy as np
 import random
 
 #from analyze_2_6 import R
@@ -14,125 +14,129 @@ import random
 #from commutative import commutative_results
 #from decomposition import decomp_results
 
-# add later features without having to rerun the entire code
-# feature will be dataframe, need to fig that out 
-def add_dat(name, feature, R):
-    i = 0
-    new_feat = []
-    for r1 in R:
-        for r2 in R:
-            new_feat.append(feature.loc[r1, r2])
+''' print responses with overall co-occurence > 100 and specified relation = 1'''
+def top_resps(df, relation):
+    return df.loc[df[relation] == 1,:].loc[df["Overall"] > 100,:]
 
-    with open(name, 'r') as read_obj, open(name, 'w', newline='') as write_obj:
-        csv_reader = reader(read_obj)
-        csv_writer = writer(write_obj)
+'''print random 10 responses with a relation with value (0 or 1)'''
+def rand_ten(df):
+    add_val_r = df.sample(n=10)
+    indices = add_val_r.index.value
 
-        for row in csv_reader:
-            if row[0] == "O":
-                row.append("Add_Decomp")
-
-            row.append(new_feat[i])
-            csv_writer.writerow(row)
-            i += 1
-
+    return indices
 
 # statsmodels.formula.api ols() results
 def results(file_name, df):
     with open(file_name, "w") as f:
-        print("MULTIPLE LINEAR REGRESSION RESULTS", file=f)
+        print("MULTIPLE LINEAR REGRESSION RESULTS (All)", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall + C(Commutativity) + C(Add_Decomp)", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
-        print("\nMULTIPLE LINEAR REGRESSION RESULTS (WITH INTERACTION)", file=f)
-        comm_sm = smf.ols(formula="Successive ~ Overall * C(Add_Decomp) + C(Commutativity)", data=df).fit()
-        print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         print("\nMULTIPLE LINEAR REGRESSION RESULTS (Only add_decomp)", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall + C(Add_Decomp)", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         print("\nMULTIPLE LINEAR REGRESSION RESULTS (Only Add_decomp + Interaction)", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall * C(Add_Decomp)", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         print("\nMULTIPLE LINEAR REGRESSION RESULTS (Only Comm)", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall + C(Commutativity)", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         print("\nMULTIPLE LINEAR REGRESSION RESULTS (Only Comm + Interaction)", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall * C(Commutativity)", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         print("\nSIMPLE LINEAR REGRESSION RESULTS", file=f)
         comm_sm = smf.ols(formula="Successive ~ Overall", data=df).fit()
         print(comm_sm.summary(), file=f)
-        print(comm_sm.rsquared)
+        #print(comm_sm.rsquared)
         if file_name == "All_results.txt":
             # return predictions using one feature
             return comm_sm.predict()
 
+'''calculate means for overall, successive co-occurrence, and predicted s.c. for response pairs split on specified relation'''
+def means(df, value):
+    means = dict(overall_mean = df["Overall"].mean(), 
+                 successive_mean = df["Successive"].mean(),
+                 predicted_mean = df["Predictions"].mean())
+    return means
+
+''' run analysis on specified item (A, B, C, D, E, or All)'''
+def print_results(file, df):
+    predictions = results(file, df)
+    return predictions
+
+# graph cooc_all and cooc_succ
+def visualize(values):
+    f = plt.figure()
+    f.set_figwidth(6)
+    f.set_figheight(3)
+
+    all_zeros = values[0]["Overall"].to_numpy()
+    succ_zeros = values[0]["Successive"].to_numpy()
+
+    all_ones = values[1]["Overall"].to_numpy()
+    succ_ones = values[1]["Successive"].to_numpy()
+
+    plt.scatter(all_zeros, succ_zeros, s=6, c='navy', marker='o')
+    plt.scatter(all_ones, succ_ones, s=6, c='dodgerblue', marker='o')
+
+    reg_ones = np.poly1d(np.polyfit(all_ones, succ_ones, 1))
+    reg_zeros = np.poly1d(np.polyfit(all_zeros, succ_zeros, 1))
+
+    plt.plot(all_zeros, reg_zeros(all_zeros), "r--", color='royalblue')
+    plt.plot(all_ones, reg_ones(all_ones),"r--", color='dodgerblue')
+
+    plt.title("Simple Linear Regression")
+    plt.xlabel("Frequency of overall co-occurrence")
+    plt.ylabel("Frequency of successive cooccurrence")
+    plt.show()
+
 # 'comm_dat': dataframe that shows if a relationship between two responses is commutative
 # 'add_d_dat': dataframe that shows if a relationship between two responses is additively decomposed
 # 'mult_d_dat': dataframe that shows if a relationship between two responses is multiplicatively decomposed
+
 # comm_dat = commutative_results(R)
-#comm_dat = pd.read_excel("ANK_commutative_relations.xlsx", index_col=0)
-#add_d_dat = decomp_results(R, "+", "add")
-#mult_d_dat = decomp_results(R, "*", "mult")
-#add_d_dat = pd.read_excel("ANK_add_decomp_relations.xlsx", index_col=0)
-#add_dat("mult_decomp", mult_d_dat, R)
+# add_d_dat = decomp_results(R, "+", "add")
 
-#analysis_dat(cooc_dat, succ_dat, comm_dat, add_d_dat, "Commutativity", "Add_Decomp")
+# analysis_dat(cooc_dat, succ_dat, comm_dat, add_d_dat, "Commutativity", "Add_Decomp")
 # analysis_dat(add_d_dat, "Additive Decomposition")
-#analysis_dat(mult_d_dat, "Multiplicative Decomposition")
+# analysis_dat(mult_d_dat, "Multiplicative Decomposition")
 
-# "df": dataframe of all the data necessary for analysis
-# columns are "Overall", "Comm", "Successive", "Predictions", "Residuals", "Add_Decomp", "Mult_decomp"
-# df = pd.read_csv("regression_dat.csv", header=0, index_col=0)
+''' df: dataframe of all the data necessary for analysis
+    columns: Overall, Commutativity, Add_Decomp, Successive, Items
+    index: string with value "resp1, resp2 '''
+df = pd.read_csv("regression_dat.csv", header=0, index_col=0)
 
-df = pd.read_csv("regression_dat.csv", index_col=0)
-
-# run analysis on all items
-predictions = results("All_results.txt", df)
+# set types 
+df = df.astype({'Overall':str, 'Commutativity':int, 'Add_Decomp':int, 'Successive':int, 'Item':str})
 
 #create a dictionary of dataframes, split on Item (A, B, C, D, or E)
-#item_spec = dict(tuple(df.groupby("Item")))
+item_spec = dict(tuple(df.groupby("Item")))
 
-# run analysis for each individual item
-#results("Item_A_results.txt", item_spec["A"])
-#results("Item_B_results.txt", item_spec["B"])
-#results("Item_C_results.txt", item_spec["C"])
-#results("Item_D_results.txt", item_spec["D"])
-#results("Item_E_results.txt", item_spec["E"])
+# example use of print_results
+predictions = print_results("All_results.txt", df)
+print_results("Item_A_results.txt", item_spec["A"])
 
 # add predictions of successive coocurrence frequency based off of overall coocurrence frequency
-#df["Predictions"] = predictions
+df["Predictions"] = predictions
 
 # add residuals of predictions 
 #df["Residuals"] = df["Successive"] - df["Predictions"]
 
-# create a dictionary of dataframes, split on Commutativity (0 or 1)
-#comm_val = dict(tuple(df.groupby("Commutativity"))) 
+# create a dictionary of dataframes, split on relations(0 or 1)
+comm_val = dict(tuple(df.groupby("Commutativity"))) 
+add_val = dict(tuple(df.groupby("Add_Decomp")))
 
-# calculate means for overall and successive co-occurrence for commutative and non-commutative response pairs
-#nc_means = dict(overall_mean = comm_val[0]["Overall"].mean(), successive_mean = comm_val[0]["Successive"].mean())
-#c_means = dict(overall_mean = comm_val[1]["Overall"].mean(), successive_mean = comm_val[1]["Successive"].mean())
+# example usage of rand_ten and top_resps
+print(rand_ten(comm_val[1]))
+print(top_resps(df, "Commutativity"))
 
-# calculate predicted mean and residual mean for commutative and non-commutative response pairs
-#nc_pr_means = dict(predict_mean = comm_val[0]["Predictions"].mean(), res_mean = comm_val[0]["Residuals"].mean())
-#c_pr_means = dict(predict_mean = comm_val[1]["Predictions"].mean(), res_mean = comm_val[1]["Residuals"].mean())
+# example use of means
+print(means(comm_val[1]))
 
-f = plt.figure()
-f.set_figwidth(6)
-f.set_figheight(3)
-
-# graph cooc_aland cooc_succ
-x = df["Overall"].to_numpy() # cooc_all
-y = df["Successive"].to_numpy() # cooc_succ
-
-plt.scatter(x, y, s=6, marker='o') # scatterplot
-plt.plot(x, 0.3905 * x - 0.0205, color='navy') # regression line
-plt.title("Simple Linear Regression")
-plt.xlabel("Frequency of overall co-occurrence")
-plt.ylabel("Frequency of successive cooccurrence")
-plt.show()
+visualize(comm_val)
+visualize(add_val)
 
