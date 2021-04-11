fda2f482051f0134a39e843203ca377b25a0809c
Carissa Garde <47163038+beesbuzzingby@users.noreply.github.com>
2021-04-11 08:43:25 -0400

    New files with all info in two files

--- analyze_2_6.py
+++ analyze_2_6.py
@@ -1,13 +1,12 @@
 import numpy as np, pandas as pd
-from numpy.core.defchararray import add
+from numpy.core.defchararray import isdigit
 import re # for regular expressions
 from collections import Counter
 
 # load raw data
-raw_dat         = pd.read_excel("ANK_raw_data.xlsx", index_col=0)
+raw_dat = pd.read_excel("ANK_raw_data.xlsx", index_col=0)
 
 # create a dict items containing source numbers and target number for each item
-# added type (dense/sparse)
 items = {
     'A': {'opts': [1,2,3,4],       'targ': 6,   'type': 'dense'},
     'B': {'opts': [2,4,8,12,32],   'targ': 16,  'type': 'dense'},
@@ -15,8 +14,8 @@ items = {
     'D': {'opts': [2,4,6,16,24],   'targ': 12,  'type': 'dense'},
     'E': {'opts': [3,5,30,120,180],'targ': 12,  'type': 'sparse'}} 
 
+''' Check whether r is a valid response for item '''
 def validate(r, item):
-    """ Check whether r is a valid response for item"""
     I = items[item]
     numbers = [int(n) for n in re.findall('(\d+)', r)]
     if len([n for n in numbers if n not in I['opts']])>0:
@@ -27,7 +26,7 @@ def validate(r, item):
         except (SyntaxError, ZeroDivisionError):
             return(False)
 
-# remove parentheses from a response if they don't affect the result
+''' Remove parentheses from a response if they don't affect the result '''
 def check_p(r):
     if "(" not in r and ")" not in r:
         return r 
@@ -41,7 +40,7 @@ def check_p(r):
     else:
         return r
 
-# check if two responses have the same target value (are in the same item)
+''' Check if two responses have the same target value (are in the same item) '''
 def item_check(r1, r2):
     targ_1, targ_2 = pd.eval(r1), pd.eval(r2)
 
@@ -54,8 +53,7 @@ def item_check(r1, r2):
         elif targ_1 == 59:
             return "C"
         elif targ_1 == 12:
-            # D and E have equal targets, so need to differentiate 
-            # options are mutually exclusive, so just need to check first number of each
+            # D and E have equal targets, so need to differentiate
             num1, num2 = 0, 0
 
             # get first number that appears in each response
@@ -80,7 +78,21 @@ def item_check(r1, r2):
     # the two responses aren't part of the same item
     return "N"
 
-# prepare data for analysis with given feature
+''' R: a list of all valid responses '''
+L = [items[item]['valid_resps'] for item in items.keys()]
+R_0 = [r for resps in L for r in resps]
+R_1 = [check_p(resp) for resp in R_0 if "+-" not in resp]
+
+R = []
+for resp in R_1:
+    if resp not in R:
+        R.append(resp)
+
+''' Prepare data for analysis with given features, store in "regression_dat.csv"
+    cooc_dat: dataframe of overall coocurrence
+    succ_dat: datafram of successive coocurrence
+    feature, feature2: features to add (currently using commutativity, additive decomposition)
+    f_name, f_name2: feature names to be used in csv'''
 def analysis_dat(cooc_dat, succ_dat, feature, feature2, f_name, f_name2):
     # dat: overall coocurrence, commutativity, successive cooccurrence, item
     # rows: each response pair
@@ -102,10 +114,10 @@ def analysis_dat(cooc_dat, succ_dat, feature, feature2, f_name, f_name2):
     df = pd.DataFrame(dat, columns = ['Overall', f_name, f_name2, 'Successive', 'Item'], index=rows)
     df.to_csv("regression_dat.csv")
             
-# for each item in items, add
-# 'resp_lists', a list containing each participant's answers in list format
-# 'valid_resp_counts', a list of valid individual answers and their frequencies
-# 'valid_resps', a list of valid individual answers without frequencies
+''' For each item in items, add
+    'resp_lists', a list containing each participant's answers in list format
+    'valid_resp_counts', a list of valid individual answers and their frequencies
+    'valid_resps', a list of valid individual answers without frequencies '''
 def calc_cooccurrence():
     for item in raw_dat.columns:
         v = list(raw_dat.loc[:,item])
@@ -116,19 +128,9 @@ def calc_cooccurrence():
         items[item]['valid_resp_counts'] = Counter(x).most_common()
         items[item]['valid_resps'] = [resp for (resp, count) in items[item]['valid_resp_counts']]
 
-    # create two co-occurrence matrices, each with one row and column per valid response
-    # 'cooc_dat': number of co-occurrences for each valid response pair
-    # 'succ_dat': number of co-occurrences in immediate succession
-    L = [items[item]['valid_resps'] for item in items.keys()]
-    R_0 = [r for resps in L for r in resps]
-    R_1 = [check_p(resp) for resp in R_0 if "+-" not in resp]
-
-    # used to create analysis dataframe of unique responses
-    R = []
-    for resp in R_1:
-        if resp not in R:
-            R.append(resp)
-
+    ''' Create two co-occurrence matrices, each with one row and column per valid response
+        'cooc_dat': number of co-occurrences for each valid response pair
+        'succ_dat': number of co-occurrences in immediate succession '''
     cooc_dat = pd.DataFrame(data=np.zeros((len(R), len(R)), dtype=int), index=R, columns=R)
     succ_dat = pd.DataFrame(data=np.zeros((len(R), len(R)), dtype=int), index=R, columns=R)
 
@@ -152,26 +154,156 @@ def calc_cooccurrence():
     cooc_dat.to_excel("ANK_cooccurrence_all.xlsx")
     succ_dat.to_excel("ANK_cooccurrence_successive.xlsx")
 
-''' print responses with overall co-occurence > 100 and specified relation = 1'''
-def top_resps(df, relation):
-    return df.loc[df[relation] == 1,:].loc[df["Overall"] > 100,:]
+''' Determine if two items are commutative
+    Commutativity definition: 
+    '''
+def is_commutative(r1, r2):
+    div_1, div_2 = " ", " "
 
-'''print random 10 responses with a relation with value (0 or 1)'''
-def rand_ten(df):
-    add_val_r = df.sample(n=10)
-    indices = add_val_r.index.value
+    # if the responses are exactly the same, they are not commutative
+    if r1 == r2:
+        return False
 
-    return indices
+    # take care of issues with division expressions having equal results but not being commutative
+    stop, sec = 0, 0
+    if (")" not in r1 and ")" not in r2) and ("/" in r1 and "/" in r2):
+        # check how many divison symbols there are and store the divisor and dividend
+        for i in range(len(r1)):
+            if i == 1 and r1[i] == "/":
+                sec = 1
+                break
+            if r1[i] == "/":
+                if stop == 1:
+                    stop = 2
+                    break
 
+                div_1 = r1[i - 1]
+                div_2 = r1[i + 1]
 
+                stop = 1
 
-''' df: dataframe of all the data necessary for analysis
-    columns: Overall, Commutativity, Add_Decomp, Successive, Items
-    index: string with value "resp1, resp2 '''
-df = pd.read_csv("regression_dat.csv", header=0, index_col=0)
+        for i in range(len(r2)):
+            # ignore resps with 2+ division symbols or resps with division as the second character
+            if stop == 2 or sec == 1:
+                break
+            
+            # if one of the divisors or dividends doesn't match the other, responses aren't commutative
+            if r2[i] == "/":
+                if div_1 != r2[i - 1]:
+                    return False
+                if div_2 != r2[i + 1]:
+                    return False 
 
-# set types 
-df = df.astype({'Overall':str, 'Commutativity':int, 'Add_Decomp':int, 'Successive':int, 'Item':str})
+    # sort each response in ASCII order 
+    rearr_r1 = ''.join(sorted(r1))
+    rearr_r2 = ''.join(sorted(r2)) 
 
-#create a dictionary of dataframes, split on Item (A, B, C, D, or E)
-item_spec = dict(tuple(df.groupby("Item")))
+    # if these responses are equal, they have to be commutative (results are guaranteed to be equal)
+    if rearr_r1 == rearr_r2:
+        return True
+
+    return False
+
+''' Create a dataframe that shows which response pairs have the relation "commutativity" '''
+def commutative_results():
+    c_d = pd.DataFrame(data=np.zeros((len(R), len(R)), dtype=int), index=R, columns=R)
+
+    for r1 in R:
+        for r2 in R:
+            if is_commutative(r1, r2):
+                # record that these responses are commutative
+                c_d.loc[r1, r2] = 1
+                c_d.loc[r2, r1] = 1
+
+    # save commutative relation data to excel file
+    c_d.to_excel("ANK_commutative_relations.xlsx")
+    
+    return c_d
+
+''' Determine if two items are related by additive decomposition.  
+    Additive decomposition definition: 
+    Two responses, which can be broken up into valid sub-expressions (or "nodes"), and which differ by one or more nodes.  
+    For one response, this node(s) contains a sub-expression of only a single number. 
+    For the other response, this same node(s) must be a sub-expression involving only addition that is equal to the sub-expression in the other response's same node(s).  
+    The order of these sub-expressions matters, and sub-expressions must be in the same location for both responses.'''
+def is_decomp(r1, r2, operator):
+    # if the two responses aren't in the same item, can't be decomposed
+    if item_check(r1, r2) == "N":
+        return False
+
+    # must have operator in the potentially "decomposed" response
+    if operator not in r2:
+        return False
+
+    decomp = "Maybe"
+    decomp_len = 0
+
+    # search for decomposition from r1 -> r2 (as in: R1 = 2 + 2, R2 = 1 + 1 + 2)
+    # therefore, r2 cannot be shorter than r1
+    if len(r2) < len(r1):
+        return False
+
+    for i in range(len(r1)):
+        sub_exp = ""
+        # start at i's position in j
+        # if decomp found this will change
+        for j in range(i, len(r2)):
+            # potential for decomposition
+            if (r2[j] != r1[i] and j == i) and ((j + 1 < len(r2) and r2[j + 1] == operator) or (j - 1 < len(r2) and r2[j - 1] == operator and r2[j + 1] != operator)):
+                k = j
+                while k < len(r2):
+                    sub_exp += r2[k]
+                    print(sub_exp)
+                    
+                    # do the stuffs
+                    if isdigit(sub_exp[-1]):
+                        if pd.eval(sub_exp) == int(r1[i]):
+                            decomp_len += len(sub_exp)
+                            decomp = "True"
+                            break
+                        elif (k == len(r2) - 1 or (isdigit(r2[k]) and r2[k + 1] != "+")) and decomp == "Maybe":
+                            decomp = "False"
+                            j += k - j
+                            break
+                        else:
+                            decomp = "Maybe"     
+                    k += 1
+            if decomp == "True":
+                break
+
+            # These expressions don't match up, can't be decomposed
+            if decomp == "False" and ((j == i and r2[j] != r1[i]) or (j == i + len(sub_exp))):
+                return False
+            # These expressions have at least one case of decomposition
+            # but we need to make sure there are no other differences
+            elif decomp == True and j == i + len(sub_exp):
+                if r2[j] != r1[i]:
+                    # the expressions have another difference
+                    return False
+
+    # The expressions only differ by one or more cases of decomposition, all good
+    return True
+
+''' Create a dataframe that shows which response pairs have the relation "additive decomposition" '''
+def decomp_results(operator, op_name):
+    d_d = pd.DataFrame(data=np.zeros((len(R), len(R)), dtype=int), index=R, columns=R)
+
+    i = 0
+    ignore = []
+    
+    for r1 in R:
+        for r2 in R:
+            print(i)
+            if [r1, r2] in ignore:
+                    continue
+            if is_decomp(r1, r2, operator):
+                # record that these responses are commutative and don't rerun with them reversed
+                d_d.loc[r1, r2] = 1
+                d_d.loc[r2, r1] = 1
+                ignore.append([r2, r1])
+            i += 1
+
+    # save commutative relation data to excel file
+    d_d.to_excel("ANK_" + op_name + "_decomp_relations.xlsx")
+    
+    return d_d
--- linear_reg_analysis_2_6.py
+++ linear_reg_analysis_2_6.py
@@ -1,31 +1,15 @@
-import pandas as pd
+import pandas as pd, numpy as np
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
-from pprint import pprint
-from csv import reader
-from csv import writer
-import numpy as np
-import random
 
-#from analyze_2_6 import R
 #from analyze_2_6 import analysis_dat
-#from analyze_2_6 import cooc_dat
-#from analyze_2_6 import succ_dat
-#from commutative import commutative_results
-#from decomposition import decomp_results
+#from analyze_2_6 import commutative_results
+#from analyze_2_6 import decomp_results
 
-''' print responses with overall co-occurence > 100 and specified relation = 1'''
-def top_resps(df, relation):
-    return df.loc[df[relation] == 1,:].loc[df["Overall"] > 100,:]
-
-'''print random 10 responses with a relation with value (0 or 1)'''
-def rand_ten(df):
-    add_val_r = df.sample(n=10)
-    indices = add_val_r.index.value
-
-    return indices
-
-# statsmodels.formula.api ols() results
+''' Run analysis on specified item (A, B, C, D, E, or All) 
+    uses statsmodel.formula.api regression 
+    file_name: file results are printed to
+    df: dataframe of data from regression_dat.csv, or a subsection of that data split on Item '''
 def results(file_name, df):
     with open(file_name, "w") as f:
         print("MULTIPLE LINEAR REGRESSION RESULTS (All)", file=f)
@@ -52,23 +36,23 @@ def results(file_name, df):
         comm_sm = smf.ols(formula="Successive ~ Overall", data=df).fit()
         print(comm_sm.summary(), file=f)
         #print(comm_sm.rsquared)
+
         if file_name == "All_results.txt":
             # return predictions using one feature
             return comm_sm.predict()
 
-'''calculate means for overall, successive co-occurrence, and predicted s.c. for response pairs split on specified relation'''
-def means(df, value):
+''' Calculate means for overall, successive co-occurrence, and predicted s.c. 
+   for response pairs split on specified relation '''
+def means(df):
     means = dict(overall_mean = df["Overall"].mean(), 
                  successive_mean = df["Successive"].mean(),
                  predicted_mean = df["Predictions"].mean())
     return means
 
-''' run analysis on specified item (A, B, C, D, E, or All)'''
-def print_results(file, df):
-    predictions = results(file, df)
-    return predictions
-
-# graph cooc_all and cooc_succ
+''' Graph successive coocurrence based on overall coocurrence
+    values: the relation to split the points (ex: comm, or add_decomp)
+    navy/royalblue: pairs without specified relation and their regression line
+    dodgerblue: pairs with specified relation and their regression line'''
 def visualize(values):
     f = plt.figure()
     f.set_figwidth(6)
@@ -80,13 +64,17 @@ def visualize(values):
     all_ones = values[1]["Overall"].to_numpy()
     succ_ones = values[1]["Successive"].to_numpy()
 
+    # plot points without specified relation
     plt.scatter(all_zeros, succ_zeros, s=6, c='navy', marker='o')
+    # plot points with specified relation
     plt.scatter(all_ones, succ_ones, s=6, c='dodgerblue', marker='o')
 
     reg_ones = np.poly1d(np.polyfit(all_ones, succ_ones, 1))
     reg_zeros = np.poly1d(np.polyfit(all_zeros, succ_zeros, 1))
 
+    # plot regression line for points without specified relation
     plt.plot(all_zeros, reg_zeros(all_zeros), "r--", color='royalblue')
+     # plot regression line for points with specified relation
     plt.plot(all_ones, reg_ones(all_ones),"r--", color='dodgerblue')
 
     plt.title("Simple Linear Regression")
@@ -94,49 +82,51 @@ def visualize(values):
     plt.ylabel("Frequency of successive cooccurrence")
     plt.show()
 
-# 'comm_dat': dataframe that shows if a relationship between two responses is commutative
-# 'add_d_dat': dataframe that shows if a relationship between two responses is additively decomposed
-# 'mult_d_dat': dataframe that shows if a relationship between two responses is multiplicatively decomposed
 
+''' 'comm_dat': dataframe that shows if a relationship between two responses is commutative'''
 # comm_dat = commutative_results(R)
+
+''' 'add_d_dat': dataframe that shows if a relationship between two responses is additively decomposed '''
 # add_d_dat = decomp_results(R, "+", "add")
 
-# analysis_dat(cooc_dat, succ_dat, comm_dat, add_d_dat, "Commutativity", "Add_Decomp")
-# analysis_dat(add_d_dat, "Additive Decomposition")
-# analysis_dat(mult_d_dat, "Multiplicative Decomposition")
+# add data to dataframe and "regression_dat.csv" for analysis
+#analysis_dat(cooc_dat, succ_dat, comm_dat, add_d_dat, "Commutativity", "Add_Decomp")
 
 ''' df: dataframe of all the data necessary for analysis
     columns: Overall, Commutativity, Add_Decomp, Successive, Items
     index: string with value "resp1, resp2 '''
 df = pd.read_csv("regression_dat.csv", header=0, index_col=0)
 
-# set types 
+# set types of each column 
 df = df.astype({'Overall':str, 'Commutativity':int, 'Add_Decomp':int, 'Successive':int, 'Item':str})
 
-#create a dictionary of dataframes, split on Item (A, B, C, D, or E)
+''' item_spec: create a dictionary of dataframes, split on Item (A, B, C, D, or E) '''
 item_spec = dict(tuple(df.groupby("Item")))
 
-# example use of print_results
-predictions = print_results("All_results.txt", df)
-print_results("Item_A_results.txt", item_spec["A"])
+# run the analysis
+predictions = results("All_results.txt", df)
 
 # add predictions of successive coocurrence frequency based off of overall coocurrence frequency
-df["Predictions"] = predictions
+#df["Predictions"] = predictions
 
-# add residuals of predictions 
+# run the analysis for each item individually
+# print_results("Item_A_results.txt", item_spec["A"])
+# print_results("Item_A_results.txt", item_spec["B"])
+# print_results("Item_A_results.txt", item_spec["C"])
+# print_results("Item_A_results.txt", item_spec["D"])
+# print_results("Item_A_results.txt", item_spec["E"])
+
+# add residuals of predictions
 #df["Residuals"] = df["Successive"] - df["Predictions"]
 
 # create a dictionary of dataframes, split on relations(0 or 1)
 comm_val = dict(tuple(df.groupby("Commutativity"))) 
 add_val = dict(tuple(df.groupby("Add_Decomp")))
 
-# example usage of rand_ten and top_resps
-print(rand_ten(comm_val[1]))
-print(top_resps(df, "Commutativity"))
-
-# example use of means
+# example use of printing out the mean co-occurrence values 
 print(means(comm_val[1]))
 
+# show a graph of commutative pairs + regression line, and non-commutative pairs + regression line
 visualize(comm_val)
+# show a graph of add-decomp pairs + regression line, and non-add-decomp pairs + regression line
 visualize(add_val)
-
